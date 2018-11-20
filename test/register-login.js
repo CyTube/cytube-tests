653dc51fe79b080164af97c71ef7c8a3001b17c8
Calvin Montgomery <calzoneman@gmail.com>
2018-11-19 22:44:41 -0800

    Add initial HTTP client and basic login test

---
+++ test/register-login.js
@@ -0,0 +1,19 @@
+require('../lib/logger').init();
+const assert = require('assert');
+const { HTTPClient } = require('../lib/http-client');
+
+describe('RegisterLoginTest', () => {
+    let client = new HTTPClient(process.env.HOST, parseInt(process.env.PORT, 10));
+
+    it('registers a user and logs in as that user', async () => {
+        let name = `X${Date.now()}`;
+        let password = 'test';
+
+        await client.register({ name, password });
+        await client.login(name, password);
+
+        assert(client.getCookie('auth'), 'Expected auth cookie after logging in');
+        // TODO: probably also want to validate that requesting a page, e.g.
+        // the index, shows the logged in banner
+    });
+});
